2517133b425d6d8933f364ff556e2e030bb866ce
Dale Wick <dmwick@yahoo.com>
2026-04-07 21:46:02 -0400

    Faster opaque text

--- src/main.c
+++ src/main.c
@@ -332,8 +332,8 @@ void draw_big_text(const char *text, uint16_t x, uint8_t y, uint8_t color)
 void draw_text_pixel(uint16_t x, uint8_t y,uint8_t color)
 {
     //int offset = (x/16)*256+y*16+(x%16);
-    int offset = ((x>>4)<<8)+(y<<4)+(x&0x0F);
-    if(offset<0 || offset>=256*20) return;  // Clip
+    uint16_t offset = ((x>>4)<<8)+(y<<4)+(x&0x0F);
+    if(offset>=256*20) return;  // Clip
     text_tiles[offset] = color;
 }
 
@@ -345,7 +345,7 @@ void draw_text_pixel_offset(uint16_t offset, uint8_t color)
 
 void draw_text_char(uint16_t x, uint8_t y, uint8_t c, uint8_t color)
 {
-    if(x&15>8&&y&15>8) {
+    if(((x&15)>8) || ((y&15)>8)) {
         // Straddling tiles, draw pixel by pixel
         const uint8_t *ptr = font+((c-32)<<3);
         for(uint8_t i=0;i<8;i++)
@@ -386,23 +386,50 @@ void draw_text(uint16_t x, uint8_t y, const char *text, uint8_t color)
     }
 }
 
+void draw_char_opaque(uint16_t x, uint8_t y, uint8_t c, uint8_t fg, uint8_t bg)
+{
+    if(c<32 || c>127) c=32;
+    const uint8_t *ptr = font+((c-32)<<3);
+    if(((x&15)<=8) && ((y&15)<=8)) {
+        // use faster offset instead.
+        uint16_t offset=((x>>4)<<8)+(y<<4)+(x&0x0F);
+
+        for(uint8_t j=0;j<8;j++) {
+            uint8_t mask=0x80;
+            uint8_t pat=ptr[j]; // get the pixel pattern
+            for(uint8_t i=0;i<8;i++) {
+                if(pat & mask)
+                    draw_text_pixel_offset(offset, fg);
+                else
+                    draw_text_pixel_offset(offset, bg);
+                offset++;
+                mask>>=1;
+            }
+            offset+=8; // 8 to move to the next line
+        }
+        return;
+    }
+            
+    for(uint8_t i=0;i<8;i++)
+    {
+        uint8_t mask = 1<<(7-i);
+        for(uint8_t j=0;j<8;j++)
+        {
+            if(ptr[j] & mask)
+                draw_text_pixel(x+i, y+j, fg);
+            else
+                draw_text_pixel(x+i, y+j, bg);
+        }
+    }
+}
+
 void draw_text_opaque(uint16_t x, uint8_t y, const char *text, uint8_t fg, uint8_t bg)
 {
     for(int i=0;i<strlen(text);i++)
     {
         uint8_t c = text[i];
-        if(c<32 || c>127) c=32;
-        for(int j=0;j<8;j++)
-        {
-            for(int k=0;k<8;k++)
-            {
-                uint8_t mask = 1<<(7-k);
-                if(font[(c-32)*8+j] & mask)
-                    draw_text_pixel(x+i*8+k, y+j, fg);
-                else
-                    draw_text_pixel(x+i*8+k, y+j, bg);
-            }
-        }
+        draw_char_opaque(x, y, c, fg, bg);
+        x+=8;
     }
 }
 
